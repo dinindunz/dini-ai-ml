38ea4ac1b36c12c863c0fe7b4cea1f3b22e1ce75
Dinindu Suriyamudali <dinindunz@gmail.com>
2025-02-16 00:21:05 +1300

    churn

--- kaggle/churn/scripts/generate_training_labels.scala
+++ kaggle/churn/scripts/generate_training_labels.scala
@@ -98,6 +98,12 @@ def calculateRenewalGap(log:mutable.WrappedArray[Row], lastExpiration: String): 
   }
   gap
 }
+
+val spark = SparkSession.builder()
+    .appName("DeltaLakeExample")
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    .getOrCreate()
  
 val data = spark
   .read
